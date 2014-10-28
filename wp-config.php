d9715ec6e574d242b590cb02971428324c2d9a95
Matt <matt@chappelstudio.com>
2014-10-28 08:25:21 -0500

    Initial Commit

---
+++ wp-config.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'z3SKwWtXin_[}~g[B-|t ^^];+f-:jom>].Z-=T|FpD^xMq-z96bl:Ck+[S,OE1!');
+define('SECURE_AUTH_KEY',  'JImvSm6q$JB@ux<o7]w[cf$X(}%s?Hb?l1L_pv1r+zx@xJS^?Xj<j|@Tx+XUl^B6');
+define('LOGGED_IN_KEY',    '4;Ux^NI=xux2-_;_t?rLWVhrBIia*D{8n>PPDj) k-Me(M|Fp+,o-I3UI>fz8=mD');
+define('NONCE_KEY',        'tw2Su>v^py6@GSUcG#b7LvgTfd;3_tCvufzSHH<&t1%K?@bxx0K,+/Y=w%1#!z*i');
+define('AUTH_SALT',        'KANc0SJ|4!tiHE6}#epbOL5hxCfo09A/FjyejGc,.?*zEuq]L0f%v9CFNBQ{fbw[');
+define('SECURE_AUTH_SALT', 'j-OF/5i$wN$TN,V*S5AzxqD=X}D4|T4~bW4fSJq,^8b8ZPpNp0-EXA[[cntm]/Nj');
+define('LOGGED_IN_SALT',   'Put>LmtOySvy#lO.`B{#qL/n,4Gbxee2):<5@9$|7zv#i^rUz+n+Q_pu+t~-u`@|');
+define('NONCE_SALT',       'lIbOFH|#18r.7+<+vbC6-k})j>|&rbbc-m r.kIWn7s~NfAqfF*}(Fl8HX4h=@Jj');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/CES/header.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package Reddle
+ * @since Reddle 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.js"></script>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed">
+	<header id="masthead" role="banner">
+
+<!--BEGIN CES CUSTOM HEADER-->
+		 <nav class="top-bar" data-topbar>
+		          
+		        <div>
+		            <ul class="title-area">            
+		            <li class="name hide-for-small">
+		             <a href="default.aspx"><img src="wp-content/themes/CES/img/home_logo.png" alt="Home Logo" width="180" height="140"/></a></li>
+		            <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+		            <li class="toggle-topbar menu-icon mobilenav"><a style="color: #C60C30;" href="#">Menu</a></li>
+		            <li id="sitetitle"><a style="color: #FFF;" href="default.aspx">City Electric Supply</a></li>
+		            </ul>
+		        </div>
+		         
+		        <section class="top-bar-section">          
+		          <div class="large-10 columns"> 
+		          
+		          
+		              <div class="navheight">
+
+
+		<asp:Panel ID="Loginpanel" runat="server" Visible="true">
+		        <ul style="display:inline-block;" id="accountlogin" class="hide-for-small right">
+		                      <li style="padding: 0px;" id="Li1" runat="server"><a style="padding: 0px;"href="login.aspx">Login</a>&nbsp;</li>
+		                      <li style="padding: 0px;"><div style="color: #FFF; padding: 7px 6px 0px 6px;">|</div>&nbsp;</li>
+		                      <li style="padding: 0px;" id="Li2" runat="server"><a style="padding-right: 15px;"href="login.aspx">Sign Up</a> </li>
+		        </ul>
+
+
+		   <!-- <a style="padding: 0px;"href="login.aspx">Login</a>&nbsp;;&nbsp  <a style="padding: 0px;"href="login.aspx">Sign Up</a> -->
+		</asp:Panel>
+
+		<asp:Panel ID="LogoutPanel" runat="server" Visible="false">
+
+
+		            <ul style="display:inline-block;" id="accountlogin" class="hide-for-small right">
+		                    <li style="padding: 0px;" id="Li7" runat="server">
+		                       <a style="padding: 0px;"href="Myaccount.aspx"> <asp:Label ID="LoginName" runat="server" Text=""></asp:Label></a>&nbsp;&nbsp;</li>
+		                      <li style="padding: 0px;" id="Li3" runat="server"><a style="padding: 0px;"href="Myaccount.aspx">My Account</a>&nbsp;</li>
+		                      <li style="padding: 0px;"><div style="color: #FFF; padding: 7px 6px 0px 6px;">|</div>&nbsp;</li>
+		                      <li style="padding: 0px;" id="Li4" runat="server"><a style="padding-right: 15px;" href="Logout.aspx">Logout</a> </li>
+		                  </ul>
+		</asp:Panel>
+
+		              </div>
+		             
+
+
+		              <div>
+		                <ul class="right">
+		                    
+		                   <li class="has-dropdown hide-for-medium-up"> <a href="#">Login | Sign Up </a> 
+		                      <ul class="dropdown">
+		                      <li style="padding: 0px;" id="Li5" runat="server"><a style="padding: 0px;"href="login.aspx">Login</a></li>
+		                      <li style="padding: 0px;" id="Li6" runat="server"><a style="padding: 0px;"href="login.aspx">Sign Up</a> </li>
+		                      </ul>  
+
+		                  <li> <a href="default.aspx">Home</a> </li>
+		                  <li class="has-dropdown"> <a href="#" class="active">Company</a>
+		                    <ul class="dropdown">
+		                      <li> <a href="aboutus.aspx">About Us</a> </li>
+		                      <li> <a href="timeline.aspx">Company Timeline</a> </li>
+		                      <li> <a href="login.aspx">Open An Account</a> </li>
+		                      <li> <a href="outreach.aspx">Company Outreach</a> </li>
+		                      <li> <a href="CEScareers..aspx">Careers</a> </li>
+		                    </ul>       
+		                  </li>
+
+		                  <li class="has-dropdown"> <a href="#">Products</a> 
+		                      <ul class="dropdown">
+		                      <li> <a href="madeinamerica.aspx">Made In America</a> </li>
+		                      <li> <a href="products.aspx">Line Card</a> </li>
+		                    </ul>       
+		                  </li>
+
+		                  <li> <a href="branchlocator.aspx">Branch Locator</a> </li>
+		                  <li> <a href="mobile.aspx">Mobile App</a> </li>
+		                  <li class="has-dropdown"> <a href="#">Resources</a>
+		                    <ul class="dropdown">
+		                      <li> <a href="calculator.aspx">Calculators</a> </li>
+		                      <li> <a href="catalogs.aspx">Catalogs</a> </li>
+		                      <li> <a href="references.aspx">Electrical References</a> </li>                   
+		                    </ul>
+		                  </li>
+		                  <li  class="has-dropdown"> <a href="#">Community</a>
+		                    <ul class="dropdown">
+		                     <li> <a href="https://www.facebook.com/cityelectricsupply" target="_blank">Facebook</a> </li> 
+		                     <li> <a href="https://twitter.com/SocialCityzen" target="_blank">Twitter</a> </li> 
+		                     <li> <a href="http://www.linkedin.com/company/city-electric-supply" target="_blank">Linkedin</a> </li>
+		                     <li> <a href="http://www.youtube.com/user/SocialCITYzen" target="_blank">Youtube</a> </li>  
+		                    </ul>
+		                  </li>     
+		                  <li> <a href="ContactUs.aspx">Contact Us</a> </li>   
+		                </ul>
+		              </div>
+		           </div>
+		        </section>
+		    </nav>
+<!--END CES CUSTOM HEADER-->
+
+	</header><!-- #masthead -->
+
+	<div id="main">
